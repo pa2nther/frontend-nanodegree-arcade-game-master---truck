98261bae946b196988e70d906d6cce9f4baba554
Patina <mizpatina@gmail.com>
2015-01-31 23:28:57 -0500

    Collision Now works and added the reset for when player wins with a message.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-// Enemies our player must avoid
+//add text to the 2 divs in index
+ document.getElementById('win').style.display='none';
+ document.getElementById('lose').style.display='none';
+ document.getElementById('timec').style.display='none';
+ var d=document.getElementById('win');
+ d.innerHTML+='<h1>You Win!Press space to play again.</h1>';
+ var l=document.getElementById('lose');
+ l.innerHTML+='<h1>You were hit. Try again</h1>';
+ // Enemies our player must avoid
+//takes 3 variables-x is x position, y is y position,s for img choice
 var Enemy = function(x,y,s) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -21,13 +30,23 @@ Enemy.prototype.update = function(dt) {
 
     //moves across width of screen,then restart
     if (this.x < 505) {
-       this.x+=this.x*dt*Math.random()*2;
+       this.x+=this.x*dt*Math.random()*8;
+       //checks for collisions
+      // console.log(this.x-player.x);
+      //                                                                                                                                                                                                                             console.log(this.y-player.y);
+       if(Math.abs(this.x-player.x)<30 && Math.abs(this.y-player.y)<42){
+       document.getElementById('lose').style.display='block';
+       player.x=230;
+       player.y=330;
+       }
 
-    }else{
-        this.x=1;
+    } else {
+        this.x=1*Math.random()*15;
+         document.getElementById('lose').style.display='none';
 
     }
-}
+
+    }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -36,61 +55,96 @@ Enemy.prototype.render = function() {
 
 // Now write your own player class
 //Player-The Star of the game
-var Player = function(x,y){
+//can take 3 variables-x is x position, y is y position, s is image choice
+var Player = function(x,y,s){
     this.sprite = 'images/char-pink-girl.png';
     this.x=x;
     this.y=y;
+    this.timetocross=0;
+
 }
 // This class requires an update(), render() and
 // a handleInput() method.
 
 // Draw the player on the screen
 Player.prototype.render = function() {
+   
     ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
 }
 
 
 //Update player
+//takes 2 variables key pressed & whether to inc/dec
 Player.prototype.update = function(r,i){
     var r=r;
     var i=i;
-    if (i==37 || i==39) {
-    if (this.x+100*r>0 && this.x+100*r<505) {
-        this.x+=100*r;
+    
+    
+    //moves player 1 block on screen right or left
+    if (i=="right" || i=="left") {
+    if (this.x+90*r>-38 && this.x+90*r<500) {
+        this.x+=90*r;
+
     }
 }
+   //moves player 1 block  if up or down key pressed
+   if (i=="up" || i=="down") {
+   if (this.y+85*r>-69 && this.y+85*r<500) {
+        this.y+=85*r;
 
+    }
+}
+   //checks if the player won by safely crossing
+   if (this.y<=-10) {
+    
+    document.getElementById('win').style.display='block';
+    
+    var t=document.getElementById('timec');
+    if(i=='space'){
+    this.y=330;
+    this.x=230;
+   document.getElementById('win').style.display='none';
+   }
+   
+   
+  
+   
+   
+   }
 }
 
 //Handle input
 Player.prototype.handleInput = function(i){
     var i=i;
     var result=0;
+    
     switch (i) {
-        case 37:
+        case "left":
         result=-1;
         break;
-        case 40:
+        case "down":
+        result=1;
+        break;
+        case "up":
         result=-1;
         break;
-        case 38:
+        case "right":
         result=1;
         break;
-        case 39:
-        result=1;
-        break;
-       player.update(result,i);
+       
 
     }
+    player.update(result,i);
 
 }
 
 
 // Now instantiate your objects.
-var p1 = new Player(235,335);
-var e1 = new Enemy(7,215);
+var p1 = new Player(230,330);
+var e1 = new Enemy(7,223);
+var e2 = new Enemy(10,135)
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [e1];
+var allEnemies = [e1,e2];
 // Place the player object in a variable called player
 var player = p1;
 
@@ -102,7 +156,8 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        32: 'space'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
