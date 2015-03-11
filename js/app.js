8bbf6c64f26ff6665302a441fb33990dd930a51a
Patina Herring <mizpatina@gmail.com>
2015-03-11 04:37:09 -0400

    Update app.js
    worked on formatting

--- js/app.js
+++ js/app.js
@@ -33,26 +33,26 @@ Enemy.prototype.update = function(dt) {
        console.log(player.collide);
        player.x=230;
        player.y=330;
-       }
+      }
 
-    } else {
+      } else {
         this.x=1*Math.random()*15;
-        }}
+      }}
 
     //handle left moving vehicles
     if(this.direction=='l'){
       if (this.x > 1) {
        this.x-=this.x*dt/.8*Math.random()*7;
        //checks for collisions
-      // console.log(this.x-player.x);
-      //                                                                                                                                                                                                                             console.log(this.y-player.y);
+       // console.log(this.x-player.x);
+                                                                                                                                                                                                                            console.log(this.y-player.y);
        if(Math.abs(this.x-player.x)<30 && Math.abs(this.y-player.y)<42){
        player.collide+=1;
        player.x=230;
        player.y=330;
        }
 
-    } else {
+     } else {
         this.x=500-Math.random()*10;
         }}
 
